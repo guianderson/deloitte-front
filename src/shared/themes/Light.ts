8fe99d19386cbb0f4eebb250310dda36efb12a84
guianderson <guilherme.anderson34@outlook.com>
2024-02-24 18:25:57 -0300

    Feat - Added Themes

--- src/shared/themes/Light.ts
+++ src/shared/themes/Light.ts
@@ -1,23 +1,23 @@
 import { createTheme } from '@mui/material/styles';
 
 export const Light = createTheme({
-    palette: {
-        primary: {
-            main: '#1976d2',
-            dark: '#004ba0',
-            light: '#4791db',
-            contrastText: '#fff',
-        },
-        secondary: {
-            main: '#26890D',
-            dark: '#00600A',
-            light: '#4CAF50',
-            contrastText: '#fff',
-        },
-        background: {
-            default: '#f4f6f8',
-            paper: '#fff',
-        },
-        mode: 'light',
+  palette: {
+    primary: {
+      main: '#1976d2',
+      dark: '#004ba0',
+      light: '#4791db',
+      contrastText: '#fff',
     },
+    secondary: {
+      main: '#26890D',
+      dark: '#00600A',
+      light: '#4CAF50',
+      contrastText: '#fff',
+    },
+    background: {
+      default: '#f4f6f8',
+      paper: '#fff',
+    },
+    mode: 'light',
+  },
 });
